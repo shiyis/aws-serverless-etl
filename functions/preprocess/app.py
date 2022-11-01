1c3b1285900f84ac8b601723f7893bb910cfb395
Shiyi Shen <shiys3447@yahoo.com>
2022-10-31 20:20:21 -0400

    udpated timeout

--- functions/preprocess/app.py
+++ functions/preprocess/app.py
@@ -1,7 +1,10 @@
 from preprocess import df_apply
 import json
+import signal 
 
 def lambda_handler(event, context):
+    timeout  = signal.alarm((context.get_remaining_time_in_millis() / 1000) - 1)
+
     return {
         "statusCode": 200,
         "body": json.dumps({
