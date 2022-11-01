f4153192bfd2ecbd498fbbc51b653394b8b1533b
Shiyi Shen <shiys3447@yahoo.com>
2022-11-01 11:22:19 -0400

    updated filtered stream

--- my_layer/python/filtered_stream.py
+++ my_layer/python/filtered_stream.py
@@ -104,24 +104,28 @@ def get_stream(set, context=None, dir="../output/"):
     header = ['id', 'text']
     logging.info("there are {} seconds left".format(int(context.get_remaining_time_in_millis() / 1000) - 1))
     while int(context.get_remaining_time_in_millis() / 1000) - 1 > 0:
-        logging.info('Testing stuff')
-        # Do work
+        try: 
+            logging.info('Testing stuff')
+            # Do work
 
 
-        with open(dir + 'out.csv', 'w',encoding='UTF8') as f:
-            writer = csv.writer(f)
+            with open(dir + 'out.csv', 'w',encoding='UTF8') as f:
+                writer = csv.writer(f)
 
-            # write the header
-            writer.writerow(header)
+                # write the header
+                writer.writerow(header)
 
-            # write the data
-            for response_line in response.iter_lines():
-                if response_line:
-                    json_response = json.loads(response_line)
-                    obj = json.dumps(json_response, indent=4, sort_keys=True)
-                    l.append(obj)
-                    data = [json_response["data"]["id"],json_response["data"]["text"]]
-                    writer.writerow(data)
+                # write the data
+                for response_line in response.iter_lines():
+                    if response_line:
+                        json_response = json.loads(response_line)
+                        obj = json.dumps(json_response, indent=4, sort_keys=True)
+                        l.append(obj)
+                        data = [json_response["data"]["id"],json_response["data"]["text"]]
+                        writer.writerow(data)
+        except TimeoutError:
+            logging.info("time is up!")
+
     print("file dir exists:", os.path.isfile(dir + 'out.csv'))
     # signal.alarm(0)# This line fixed the issue above!
     return {'statusCode': 200, 'body': l[0]["text"]}
